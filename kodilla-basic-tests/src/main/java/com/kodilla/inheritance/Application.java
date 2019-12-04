fe28f36e6ccacf4d2f8bb013e0c2bc341b6a4b75
Piotr Smereka <p.smereka@outlook.com>
2019-12-04 21:51:36 +0100

    Task 3.3 v2

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Application.java
@@ -4,5 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Car car = new Car();
         car.turnOnLights();
+        Convertible convertible = new Convertible();
+        convertible.turnOnLights();
     }
 }
--- kodilla-intro/src/main/java/TestUser.java
+++ kodilla-intro/src/main/java/TestUser.java
@@ -11,7 +11,7 @@ public class TestUser {
         User[] users = new User[]{user1, user2, user3, user4, user5, user6};
 
         int average = user1.usersAgeAndAverage(users);
-        System.out.println("Average user age" + " "+ user1.usersAgeAndAverage(users) +" " + "years.");
+        System.out.println("Average user age" + " "+ average +" " + "years.");
         System.out.println("Users below average age:");
         user1.displayUsersBelowAverageAge(users,average);
 
