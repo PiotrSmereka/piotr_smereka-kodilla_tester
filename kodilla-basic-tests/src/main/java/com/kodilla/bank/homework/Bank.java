3bc9793cdeb9c65a77594fd49000db3db0f784d3
Piotr Smereka <p.smereka@outlook.com>
2020-01-11 19:08:22 +0000

    Task 3.10 v2

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -2,12 +2,14 @@ package com.kodilla.bank.homework;
 
 public class Bank {
     private CashMachine[] cashMachines;
+
     public Bank() {
-            CashMachine cashMachine = new CashMachine();
-            CashMachine cashMachine2 = new CashMachine();
-            this.cashMachines= new CashMachine[]{cashMachine,cashMachine2};
-        }
-        public int getTotalBalance () {
+        CashMachine cashMachine = new CashMachine();
+        CashMachine cashMachine2 = new CashMachine();
+        this.cashMachines= new CashMachine[]{cashMachine,cashMachine2};
+    }
+
+    public int getTotalBalance () {
         if (cashMachines.length == 0){
             return 0;
         }
@@ -17,46 +19,60 @@ public class Bank {
         }
         return total;
     }
+
     public CashMachine[] getCashMachines(){
         return cashMachines;
     }
-    public double getNumberOfOutcomeOperations(){
-        CashMachine cashMachine=new CashMachine();
 
-       if (cashMachine.getTransactions().length==0){
-           return 0;
-       }
-       double outcome =0;
-        int i;
-        for(i=0;i<cashMachine.getTransactions().length; i++){
-            if(cashMachine.getTransactions()[i]<0) {
-                outcome += cashMachine.getTransactions()[i];
+    public int getNumberOfOutcomeOperations() {
+        CashMachine cashMachine = new CashMachine();
+        if (cashMachines.length == 0) {
+            return 0;
+        }
+        int outcome = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            if (cashMachines[i].getTransactions()[i]< 0) {
+                outcome += cashMachines[i].getTransactions()[i];
             }
         }
         return outcome;
     }
+
+    public int getNumberOfIncomeOperations(){
+        CashMachine cashMachine=new CashMachine();
+        if (cashMachine.getTransactions().length==0){
+            return 0;
+        }
+        int income =0;
+        for(int i=0;i<cashMachine.getTransactions().length; i++){
+            if(cashMachine.getTransactions()[i]>0) {
+                    income += cashMachine.getTransactions()[i];
+                }
+        }
+        return income;
+    }
+
     public double getAverageOfOutcome(){
         CashMachine cashMachine=new CashMachine();
         if(cashMachine.getTransactions().length==0){
             return 0;
         }
         double outcome=0;
-        int i;
-        for(i=0;i<cashMachine.getTransactions().length; i++){
+        for(int i=0;i<cashMachine.getTransactions().length; i++){
             if(cashMachine.getTransactions()[i]<0) {
                 outcome += cashMachine.getTransactions()[i];
             }
         }
         return outcome/cashMachine.getTransactions().length;
     }
+
     public double getAverageOfIncome(){
         CashMachine cashMachine=new CashMachine();
         if(cashMachine.getTransactions().length==0){
             return 0;
         }
         double income=0;
-        int i;
-        for(i=0;i<cashMachine.getTransactions().length; i++){
+        for(int i=0;i<cashMachine.getTransactions().length; i++){
             if(cashMachine.getTransactions()[i]>0) {
                 income += cashMachine.getTransactions()[i];
             }
--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
@@ -9,18 +9,62 @@ public class BankTestSuite {
     @Test
     public void shouldDisplayTotalBalance() {
         Bank bank = new Bank ();
-
-        int total = bank.getTotalBalance();
-        assertEquals(4, total);
+        CashMachine[] tab = bank.getCashMachines();
+        tab[0].add(10);
+        tab[0].add(-5);
+        tab[1].add(10);
+        tab[1].add(-6);
+        assertEquals(9, bank.getTotalBalance());
     }
     @Test
     public void shouldDisplayNumberOfOutcomeOperations(){
-        Bank bank = new Bank();
+        Bank bank = new Bank ();
+        CashMachine[] tab = bank.getCashMachines();
+        tab[0].add(10);
+        tab[0].add(-5);
+        tab[0].add(-5);
+        tab[1].add(10);
+        tab[1].add(-8);
+        tab[1].add(-5);
+        assertEquals(4,bank.getNumberOfOutcomeOperations());
+
 
     }
     @Test
     public void shouldDisplayNumberOfIncomeOperations(){
-        Bank bank = new Bank();
+        Bank bank = new Bank ();
+        CashMachine[] tab = bank.getCashMachines();
+        tab[0].add(10);
+        tab[0].add(-5);
+        tab[0].add(-5);
+        tab[1].add(10);
+        tab[1].add(-8);
+        tab[1].add(-5);
+        assertEquals(2,bank.getNumberOfIncomeOperations());
 
+    }
+    @Test
+    public void shouldDisplayAverageOfOutcome(){
+        Bank bank = new Bank ();
+        CashMachine[] tab = bank.getCashMachines();
+        tab[0].add(10);
+        tab[0].add(-5);
+        tab[0].add(-5);
+        tab[1].add(10);
+        tab[1].add(-5);
+        tab[1].add(-5);
+        assertEquals(-5,bank.getAverageOfOutcome());
+    }
+    @Test
+    public void shouldDisplayAverageOfIncome(){
+        Bank bank = new Bank ();
+        CashMachine[] tab = bank.getCashMachines();
+        tab[0].add(10);
+        tab[0].add(-5);
+        tab[0].add(-5);
+        tab[1].add(10);
+        tab[1].add(-8);
+        tab[1].add(-5);
+        assertEquals(10,bank.getAverageOfIncome());
     }
 }
