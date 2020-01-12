8ff8876cd33fcc1873bb0d887293f0423308ee66
Piotr Smereka <p.smereka@outlook.com>
2020-01-12 20:13:58 +0000

    Task 4.4

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -1,4 +1,56 @@
 package com.kodilla.collections.sets.homework;
 
+import java.util.Objects;
+
 public class Stamp {
+    private String stampName;
+    private double stampHeight;
+    private double stampWidth;
+    private boolean stamped;
+
+    public Stamp(String stampName, double stampHeight, double stampWidth, boolean stamped) {
+        this.stampName = stampName;
+        this.stampHeight = stampHeight;
+        this.stampWidth = stampWidth;
+        this.stamped = stamped;
+    }
+    public String getStampName() {
+        return stampName;
+    }
+    public double getStampHeight() {
+        return stampHeight;
+    }
+    public double getStampWidth() {
+        return stampWidth;
+    }
+    public boolean isStamped() {
+        return stamped;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Stamp)) return false;
+        Stamp stamp = (Stamp) o;
+        return Double.compare(stamp.getStampHeight(), getStampHeight()) == 0 &&
+                Double.compare(stamp.getStampWidth(), getStampWidth()) == 0 &&
+                isStamped() == stamp.isStamped() &&
+                Objects.equals(getStampName(), stamp.getStampName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStampName(), getStampHeight(), getStampWidth(), isStamped());
+    }
+
+    @Override
+    public String toString() {
+        return "Stamp{" +
+                "stampName='" + stampName + '\'' +
+                ", stampHeight=" + stampHeight +
+                ", stampWidth=" + stampWidth +
+                ", stamped=" + stamped +
+                '}';
+    }
 }
+
--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -1,7 +1,18 @@
 package com.kodilla.collections.sets.homework;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StampsApplication {
     public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Bird", 12, 10,true ));
+        stamps.add(new Stamp("Flower", 4, 7, false));
+        stamps.add(new Stamp("Car", 10, 9,true));
+        stamps.add(new Stamp("Bird", 12, 10, true));
 
+        System.out.println(stamps.size());
+        for (Stamp stamp : stamps)
+            System.out.println(stamp);
     }
 }
