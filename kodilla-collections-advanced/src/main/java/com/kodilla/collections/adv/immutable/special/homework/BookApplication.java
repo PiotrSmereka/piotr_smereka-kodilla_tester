4044cb5bdffb305cfa565831bedf306046a095e6
Piotr Smereka <p.smereka@outlook.com>
2020-01-13 15:21:11 +0000

    Task 5.4

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -2,6 +2,12 @@ package com.kodilla.collections.adv.immutable.special.homework;
 
 public class BookApplication {
     public static void main(String[] args) {
+     BookManager bookManager = new BookManager();
 
+     bookManager.createBook("Cars", "P. Newman");
+     bookManager.createBook("My Home", "R. Norris");
+     bookManager.createBook("New history", "T. Mal");
+
+     System.out.println(bookManager.hashCode());
     }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -2,9 +2,12 @@ package com.kodilla.collections.adv.immutable.special.homework;
 
 import com.kodilla.collections.adv.immutable.Book;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BookManager {
-    public Book createBook (String title, String author){
-        Book book = new Book("Andersen", "Great Book");
-    return book;
+    public Book createBook (String title, String author) {
+        return new Book(title, author);
     }
+    Set<Book> createBook = new HashSet<>();
 }
