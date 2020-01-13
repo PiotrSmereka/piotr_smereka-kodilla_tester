823bbe05f1de1745641eb028272da3d0a57d7fb7
Piotr Smereka <p.smereka@outlook.com>
2020-01-13 12:39:49 +0000

    Task 5.2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -1,4 +1,54 @@
 package com.kodilla.collections.adv.maps.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
 public class School {
+   private String schoolName;
+
+    private List<Double> pupilsInClass = new ArrayList<>();
+
+    public School (String schoolName, double ... pupilsInClass) {
+        for (double pupils : pupilsInClass)
+            this.pupilsInClass.add(pupils);
+            this.schoolName = schoolName;
+    }
+    public double getAllPupils() {
+        double sum =0.0;
+        for (double pupils : pupilsInClass)
+            sum += pupils;
+        return sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof School)) return false;
+        School school = (School) o;
+        return Objects.equals(schoolName, school.schoolName) &&
+                Objects.equals(pupilsInClass, school.pupilsInClass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(schoolName, pupilsInClass);
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public List<Double> getPupilsInClass() {
+        return pupilsInClass;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "schoolName='" + schoolName + '\'' +
+                ", pupilsInClass=" + pupilsInClass +
+                '}';
+    }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -1,7 +1,28 @@
 package com.kodilla.collections.adv.maps.homework;
 
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SchoolDirectory {
     public static void main(String[] args) {
+        Map<Principal, School> principalSchool = new HashMap<>();
+        Principal john = new Principal ("John", "Stevenson");
+        Principal kevin = new Principal ("Kevin", "Nolan");
+        Principal alan = new Principal ("Alan", "Bol");
 
+        School johnSchool = new School ("Crescent International School",28 , 25, 30, 24);
+        School kevinSchool = new School ("Modern Private School", 10, 12, 14, 14);
+        School alanSchool = new School ("Bright Riders School", 25, 23, 26, 24);
+
+        principalSchool.put (john, johnSchool);
+        principalSchool.put (kevin, kevinSchool);
+        principalSchool.put (alan, alanSchool);
+
+        for (Map.Entry<Principal, School> principalEntry : principalSchool.entrySet())
+            System.out.println("Principal: " + principalEntry.getKey().getFirstname() + " " +
+                    principalEntry.getKey().getLastname() + ", School Name: " + principalEntry.getValue().getSchoolName() +
+                    ", Total pupil numbers: " + principalEntry.getValue().getAllPupils());
     }
 }
