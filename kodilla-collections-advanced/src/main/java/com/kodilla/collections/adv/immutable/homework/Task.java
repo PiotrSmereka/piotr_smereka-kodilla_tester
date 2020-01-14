3076c3c8c123af4d89f535dfc6d9e2f9dc336de2
Piotr Smereka <p.smereka@outlook.com>
2020-01-14 20:13:49 +0000

    Task 5.3 v1

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -1,16 +1,16 @@
 package com.kodilla.collections.adv.immutable.homework;
 
 public final class Task {
-    protected final String Title;
+    protected final String title;
     protected final int duration;
 
     public Task(String title, int duration) {
-        Title = title;
+        this.title = title;
         this.duration = duration;
     }
 
     public final String getTitle() {
-        return Title;
+        return title;
     }
 
     public final int getDuration() {
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/hg.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class hg {
+    public static void main(String[] args) {
+        Task task = new Task("gg", 12);
+        task.
+    }
+}
