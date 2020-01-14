7173a2996f17b98c05e4582657b49203f3d9b7fd
Piotr Smereka <p.smereka@outlook.com>
2020-01-14 20:16:12 +0000

    Task 5.3 v1

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/hg.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.collections.adv.immutable.homework;
-
-public class hg {
-    public static void main(String[] args) {
-        Task task = new Task("gg", 12);
-        task.
-    }
-}
