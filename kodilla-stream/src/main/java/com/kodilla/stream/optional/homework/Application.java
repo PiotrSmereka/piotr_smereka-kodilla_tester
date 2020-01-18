bf62c9e42ed75b337b07703034e8aa365660e85d
Piotr Smereka <p.smereka@outlook.com>
2020-01-18 10:58:01 +0000

    Task 6.2 v1

--- kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/optional/homework/Application.java
@@ -6,21 +6,27 @@ import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
-        Teacher teacher1 = new Teacher ("Oscar Wide");
-        Teacher teacher2 = new Teacher ("Martin Ferguson");
-        Teacher teacher = null;
+        Teacher teacher1 = new Teacher("Oscar Wide");
+        Teacher teacher2 = new Teacher("Martin Ferguson");
+
 
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("Alex",teacher1));
-        studentList.add(new Student("Robert",null));
-        studentList.add(new Student ("Eva",teacher2));
+        studentList.add(new Student("Alex", teacher1));
+        studentList.add(new Student("Robert", null));
+        studentList.add(new Student("Eva", teacher2));
         studentList.add(new Student("Maria", null));
 
-        for (Student student: studentList) {
-            System.out.println("Student :" +student.getName()+ ", " + student.teacher);
-        }
-        Optional<Teacher> optionalTeacher = Optional.ofNullable(teacher);
-        Teacher name = optionalTeacher.orElse(new Teacher(""));
-        System.out.println("<undefined>");
+        for (Student student : studentList) {
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(student.teacher);
+            optionalTeacher.ifPresentOrElse(u -> {
+                        System.out.println("Student :" + student.getName() + ", " + student.teacher);
+                    },
+                    () -> {
+                        System.out.println("Student :" + student.getName() + ", " + "Teacher :<undefined>");
+                    });
+             }
+         }
     }
-}
+
+
+
